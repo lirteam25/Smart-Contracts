18dca6d33a31ca4da0bd705717338ecaa72045e2
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-01-25 16:28:31 +0100

    update

--- unitTests/upgradeNFTMarketplace.js
+++ unitTests/upgradeNFTMarketplace.js
@@ -1,11 +1,18 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-    const proxyAddress = '0x405c2520d74b5e8df51668f62a9c598d29f465e7' // "0x405c2520d74b5e8df51668f62a9c598d29f465e7";  Replace with your proxy address
-    // const currentImplementationAddress = "0xf8c098542BF4E18dc21f1Cd4AA3b7D9BA778fBC4"; // Replace with the current implementation address
+    const proxyAddress = '0x5f5f41df03c52c47067543c5534800204ad0a634' // "0x405c2520d74b5e8df51668f62a9c598d29f465e7";  Replace with your proxy address
+    const currentImplementationAddress = "0x760772dc4923cace9765B293a43FBa5003fBEf69"; // Replace with the current implementation address
+
+    const NFTMarketplace = await ethers.getContractFactory("NFTMarketplaceUpgradable");
+    console.log("Preparing to upgrade NFTMarketplace...");
 
     const NFTMarketplaceV2 = await ethers.getContractFactory("NFTMarketplaceUpgradableV2");
-    console.log("Preparing to upgrade NFTMarketplace...");
+
+    // // Force import the existing proxy
+    await upgrades.forceImport(proxyAddress, NFTMarketplace, { implementation: currentImplementationAddress });
+
+    console.log("Upgrading NFTMarketplace...");
 
     // Perform the upgrade
     const upgraded = await upgrades.upgradeProxy(proxyAddress, NFTMarketplaceV2);
