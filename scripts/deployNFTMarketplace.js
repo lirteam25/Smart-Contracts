355bbcf05ad799b19296c6e1764637ec9b38fa24
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-01-26 13:17:09 +0100

    Exchange rate address

--- scripts/deployNFTMarketplace.js
+++ scripts/deployNFTMarketplace.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require('hardhat');
 const { getImplementationAddress } = require('@openzeppelin/upgrades-core');
 
 async function main() {
-    const exchangeRateAddress = "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada";
+    const exchangeRateAddress = "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0";
     //0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
     //0xAB594600376Ec9fD91F8e885dADF0CE036862dE0
     const NFTMarketplace = await ethers.getContractFactory("NFTMarketplaceUpgradable");
