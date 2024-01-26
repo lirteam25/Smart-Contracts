47217467e1b4eacf9e1161b0dfafce8d59f5476d
gianmariacarnazzi <gianmaria.carnazzi@gmail.com>
2024-01-26 12:46:56 +0100

    Test

--- scripts/upgradeNFTMarketplace.js
+++ scripts/upgradeNFTMarketplace.js
@@ -1,8 +1,8 @@
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-    const proxyAddress = '0x5f5f41df03c52c47067543c5534800204ad0a634' // "0x405c2520d74b5e8df51668f62a9c598d29f465e7";  Replace with your proxy address
-    const currentImplementationAddress = "0x760772dc4923cace9765B293a43FBa5003fBEf69"; // Replace with the current implementation address
+    const proxyAddress = '0x2c783c72f46b1e6213a2cba0d92e9e3711b694ca' // "0x405c2520d74b5e8df51668f62a9c598d29f465e7";  Replace with your proxy address
+    const currentImplementationAddress = "0xf8c098542BF4E18dc21f1Cd4AA3b7D9BA778fBC4"; // Replace with the current implementation address
 
     const NFTMarketplace = await ethers.getContractFactory("NFTMarketplaceUpgradable");
     console.log("Preparing to upgrade NFTMarketplace...");
